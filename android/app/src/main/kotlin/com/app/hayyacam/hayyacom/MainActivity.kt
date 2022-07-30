d3fddaa891cde7236372a23fdf2f545f055bc9e6
Danish-Git <danishwasim1995@gmail.com>
2022-07-31 01:31:08 +0530

    Project Set-Up Commit

---
+++ android/app/src/main/kotlin/com/app/hayyacam/hayyacom/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.app.hayyacam.hayyacom
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'package:hayyacom/utils/utils.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
+
+import 'utils/routes/pages.dart';
+
+void main() {
+  runApp(const Hayyacom());
+}
+
+class Hayyacom extends StatelessWidget {
+  const Hayyacom({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+    return ResponsiveSizer(builder: (context, orientation, screenType) =>
+      GetMaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Hayyacom',
+        defaultTransition: Transition.rightToLeft,
+        locale: const Locale('en', 'US'),
+      )
+    );
+  }
+}
